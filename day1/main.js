a9a68e85fc15b0f5f7655820d20c6c485da4f60e
Nicolas Bauer <nicolaswilliam.bauer@gmail.com>
2022-12-10 14:16:28 -0800

    chore: lint

--- day1/main.js
+++ day1/main.js
@@ -1,17 +1,20 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const input = fs.readFileSync('./input.txt').toString();
-const caloriesPerElfStrings = input.split('\n\n');
-const caloriesPerElf = caloriesPerElfStrings.map((elfCaloriesString) => elfCaloriesString.split('\n'));
+const input = fs.readFileSync("./input.txt").toString();
+const caloriesPerElfStrings = input.split("\n\n");
+const caloriesPerElf = caloriesPerElfStrings.map((elfCaloriesString) =>
+  elfCaloriesString.split("\n")
+);
 
 const totalElfCalories = caloriesPerElf.map((calories) => {
-  return calories.reduce((total, calories) => total+parseInt(calories), 0);
-})
+  return calories.reduce((total, calories) => total + parseInt(calories), 0);
+});
 
-const sortedElfCalories = totalElfCalories.sort((a,b) => b-a);
+const sortedElfCalories = totalElfCalories.sort((a, b) => b - a);
 
 const maxCalories = sortedElfCalories[0];
-console.log('Part 1 answer:', maxCalories);
+console.log("Part 1 answer:", maxCalories);
 
-const topThreeCalories = sortedElfCalories[0] + sortedElfCalories[1] + sortedElfCalories[2];
-console.log('Part 2 answer:', topThreeCalories);
+const topThreeCalories =
+  sortedElfCalories[0] + sortedElfCalories[1] + sortedElfCalories[2];
+console.log("Part 2 answer:", topThreeCalories);
