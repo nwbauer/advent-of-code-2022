b0586f95410bafb2ff53c3af146c9d12ffc6a032
Nicolas Bauer <nicolaswilliam.bauer@gmail.com>
2022-12-11 09:11:42 -0800

    refactor: readability

--- day1/main.js
+++ day1/main.js
@@ -2,19 +2,21 @@ const fs = require("fs");
 
 const input = fs.readFileSync("./input.txt").toString();
 const caloriesPerElfStrings = input.split("\n\n");
-const caloriesPerElfArray = caloriesPerElfStrings.map((elfCaloriesString) =>
-  elfCaloriesString.split("\n")
-);
+const caloriesPerElf = caloriesPerElfStrings.map((elfCaloriesString) => {
+  const calories = elfCaloriesString.split("\n");
+  const totalCalories = calories.reduce((total, calories) => total + parseInt(calories), 0);
+  return {
+    elfCaloriesString,
+    calories,
+    totalCalories
+  }
+}).sort((a, b) => b.totalCalories - a.totalCalories);
 
-const totalCaloriesPerElf = caloriesPerElfArray.map((calories) => {
-  return calories.reduce((total, calories) => total + parseInt(calories), 0);
-});
-
-const sortedElfCalories = totalCaloriesPerElf.sort((a, b) => b - a);
-
-const maxCalories = sortedElfCalories[0];
-console.log("Part 1 answer:", maxCalories);
-
+console.log('caloriesPerElf', caloriesPerElf);
+const maxCalories = caloriesPerElf[0].totalCalories;
 const topThreeCalories =
-  sortedElfCalories[0] + sortedElfCalories[1] + sortedElfCalories[2];
-console.log("Part 2 answer:", topThreeCalories);
+  caloriesPerElf[0].totalCalories + caloriesPerElf[1].totalCalories + caloriesPerElf[2].totalCalories;
+
+console.log("solution:", maxCalories);  
+console.log("[part 1] elf with max calories:", maxCalories);
+console.log("[part 2] sum of top three calorie elfs:", topThreeCalories);
